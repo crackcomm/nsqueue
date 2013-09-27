01630068cda40b7d71568e85d458f32e64cf66a3
crackcomm <crackcomm@gmail.com>
2013-09-27 03:00:41 +0200

    Expose producer methods

--- producer/default.go
+++ producer/default.go
@@ -6,6 +6,10 @@ import (
 
 var DefaultProducer = new(Producer)
 
+func Publish(topic string, body []byte) (int32, []byte, error) {
+	return DefaultProducer.Publish(topic, body)
+}
+
 func PublishJsonAsync(topic string, v interface{}, doneChan chan *nsq.WriterTransaction, args ...interface{}) error {
 	return DefaultProducer.PublishJsonAsync(topic, v, doneChan, args...)
 }
@@ -16,6 +20,14 @@ func PublishJson(topic string, v interface{}) (int32, []byte, error) {
 
 func PublishAsync(topic string, body []byte, doneChan chan *nsq.WriterTransaction, args ...interface{}) error {
 	return DefaultProducer.PublishAsync(topic, body, doneChan, args...)
+}
+
+func MultiPublish(topic string, body [][]byte) (int32, []byte, error) {
+	return DefaultProducer.MultiPublish(topic, body)
+}
+
+func MultiPublishAsync(topic string, body [][]byte, doneChan chan *nsq.WriterTransaction, args ...interface{}) error {
+	return DefaultProducer.MultiPublishAsync(topic, body, doneChan, args...)
 }
 
 func Connect(addr string) {
