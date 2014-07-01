c8de32a7c5072a89d36a9bc011369154335f6818
dmadrigalejos <dmadrigalejos@zvelo.com>
2014-07-01 17:17:19 +0800

    Added more consumer unit test

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -8,13 +8,13 @@ import (
 var (
 	topic = "consumerTestTopic"
 	channel = "consumerTestChannel"
-	maxInFlight = 1
+	maxInFlight = 3
 
 	lookupdHTTPAddr = "127.0.0.1:4161"
-	lookupdHTTPAddrs = []string{"127.0.0.1:4161"}
+	lookupdHTTPAddrs = []string{"127.0.0.2:4161"}
 
 	destNsqdTCPAddr = "127.0.0.1:4150"
-	destNsqdTCPAddrs = []string{"127.0.0.1:4150"}
+	destNsqdTCPAddrs = []string{"127.0.0.2:4150"}
 
 	debug = true
 )
@@ -30,6 +30,13 @@ func TestRegister(t *testing.T) {
 			So(err, ShouldEqual, nil)
 		})
 	})
+
+	Convey("Given wrong topic, channel", t, func() {
+		Convey("It should produce an error", func() {
+			err := Register("", "", maxInFlight, mgsHandle)
+			So(err, ShouldNotEqual, nil)
+		})
+	})
 }
 
 func TestConnectLookupd(t *testing.T) {
@@ -37,6 +44,13 @@ func TestConnectLookupd(t *testing.T) {
 		Convey("It should not produce any error", func() {
 			err := ConnectLookupd(lookupdHTTPAddr)
 			So(err, ShouldEqual, nil)
+		})
+	})
+
+	Convey("Given wrong lookupd address", t, func() {
+		Convey("It should produce an error", func() {
+			err := ConnectLookupd("127.0.0.1")
+			So(err, ShouldNotEqual, nil)
 		})
 	})
 }
@@ -48,33 +62,43 @@ func TestConnectLookupdList(t *testing.T) {
 			So(err, ShouldEqual, nil)
 		})
 	})
+
+	Convey("Given list of wrong lookupd address", t, func() {
+		Convey("It should not produce an error", func() {
+			err := ConnectLookupdList([]string{"127.0.0.2"})
+			So(err, ShouldNotEqual, nil)
+		})
+	})
 }
 
 func TestConnect(t *testing.T) {
 	Convey("Given nsqd address", t, func() {
-		Convey("It should not produce any error", nil )
-		//	err := Connect(destNsqdTCPAddr)
-		//	So(err, ShouldEqual, nil)
-		//})
+		Convey("It should not produce any error", func() {
+			err := Connect(destNsqdTCPAddr)
+			So(err, ShouldEqual, nil)
+		})
+	})
+
+	Convey("Given wrong nsqd address", t, func() {
+		Convey("It should produce an error", func() {
+			err := Connect("127.0.0.1")
+			So(err, ShouldNotEqual, nil)
+		})
 	})
 }
 
 func TestConnectList(t *testing.T) {
 	Convey("Given list of nsqd address", t, func() {
-		Convey("It should not produce any error", nil )
-		//	err := ConnectList(destNsqdTCPAddrs)
-		//	So(err, ShouldEqual, nil)
-		//})
+		Convey("It should not produce any error", func() {
+			err := ConnectList(destNsqdTCPAddrs)
+			So(err, ShouldEqual, nil)
+		}) 
+	})
+
+	Convey("Given list of wrong nsqd address", t, func() {
+		Convey("It should produce an error", func() {
+			err := ConnectList(destNsqdTCPAddrs)
+			So(err, ShouldNotEqual, nil)
+		}) 
 	})
 }
-
-func TestStart(t *testing.T) {
-	Convey("Given nsqd connection", t, func() {
-		Convey("It should not produce any error", nil )
-		//	err := Start(debug)
-		//	So(err, ShouldEqual, nil)
-		//})
-	})
-}
-
-
