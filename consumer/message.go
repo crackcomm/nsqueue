ddffa0883e663537a5760ad7587f1305e3abe64c
crackcomm <crackcomm@gmail.com>
2014-07-29 03:41:01 +0200

    Message.Finish(bool) returns no error

--- consumer/message.go
+++ consumer/message.go
@@ -18,29 +18,25 @@ func (m *Message) ReadJSON(v interface{}) error {
 }
 
 // GiveUp - Finish message with success state because message never will be possible to process
-func (m *Message) GiveUp() error {
+func (m *Message) GiveUp() {
 	m.Finish(true)
-	return nil
 }
 
 // Success - Finish message as successfully proccessed
-func (m *Message) Success() error {
+func (m *Message) Success() {
 	m.Finish(true)
-	return nil
 }
 
 // Fail - Mark message as failed to process
-func (m *Message) Fail() error {
+func (m *Message) Fail() {
 	m.Finish(false)
-	return nil
 }
 
 // Finish - Finish processing message
-func (m *Message) Finish(success bool) error {
+func (m *Message) Finish(success bool) {
 	if success {
 		m.Message.Finish()
 	} else {
 		m.Message.Requeue(-1)
 	}
-	return nil
 }
